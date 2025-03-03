fdc52aff34a7a85df2315461c4e6e1776ae5f5a7
Patrick <patrickjwahl@gmail.com>
2025-03-02 21:26:09 -0500

    Fix divisions order issue

--- src/shared/util/functions.ts
+++ src/shared/util/functions.ts
@@ -60,7 +60,7 @@ export const divisionSort = (a: string, b: string) => {
     return divisionsOrder.indexOf(a) - divisionsOrder.indexOf(b)
 }
 
-export const partitionsOrder = divisionsOrder.concat(Object.keys(friendlyGPA).sort()).concat(divisionsOrder.flatMap(div => Object.keys(friendlyGPA).sort().map(gpa => `_${div}_${gpa}`)))
+export const partitionsOrder = divisionsOrder.map(key => `_${key}`).concat(Object.keys(friendlyGPA).sort().map(key => `_${key}`)).concat(divisionsOrder.flatMap(div => Object.keys(friendlyGPA).sort().map(gpa => `_${div}_${gpa}`)))
 
 export const partitionSort = (a: string, b: string) => {
     return partitionsOrder.indexOf(a) - partitionsOrder.indexOf(b)
